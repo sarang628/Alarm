9030f659e0a019e46f34b970d8b9b9f970009e0d
SRYANG <sarang628@naver.com>
2023-04-13 19:14:28 +0900

    update

--- library/src/main/java/com/sarang/screen_alarm2/AlarmViewModel.kt
+++ library/src/main/java/com/sarang/screen_alarm2/AlarmViewModel.kt
@@ -1,7 +1,5 @@
 package com.sarang.screen_alarm2
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,38 +10,17 @@ class AlarmViewModel @Inject constructor(
     //private val alarmRepository: AlarmRepository
 ) :
     ViewModel() {
-    private var _isLoaded = MutableLiveData(false)
-    private var isLoaded: LiveData<Boolean> = _isLoaded
-    private val _alarms = MutableLiveData<ArrayList<AlarmUiState>>()
-    val alarms: LiveData<ArrayList<AlarmUiState>> = _alarms
-
     private val _alarmUiState = MutableLiveData<AlarmUiState>();
     val alarmUiState = _alarmUiState;
 
     /** 로그인 여부 */
     //val isLogin: LiveData<Boolean> = alarmRepository.isLogin
 
-    val hasAlarm1 = MediatorLiveData<Boolean>()
-
-    init {
-        hasAlarm1.addSource(isLoaded) {
-            hasAlarm1.value = handleHasAlarm()
-        }
-        hasAlarm1.addSource(_alarms) {
-            hasAlarm1.value = handleHasAlarm()
-        }
-    }
-
-    private fun handleHasAlarm(): Boolean {
-        return !(_isLoaded.value == true && _alarms.value?.size == 0)
-    }
-
     /**
      * 알림 불러오기
      */
     suspend fun loadAlarms() {
         try {
-            _isLoaded.postValue(true)
             /*_alarms.postValue(
                 alarmRepository.loadAlarm()
             )*/
@@ -51,12 +28,4 @@ class AlarmViewModel @Inject constructor(
             _alarmUiState.postValue(_alarmUiState.value?.copy(isRefreshing = false))
         }
     }
-
-    fun hasAlarm(): LiveData<Boolean> {
-        if (_isLoaded.value == true && _alarms.value?.size == 0) {
-            return MutableLiveData(false);
-        }
-        return MutableLiveData(true);
-    }
-
 }
