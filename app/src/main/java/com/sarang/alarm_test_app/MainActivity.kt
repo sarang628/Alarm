6f1f853a8dc3126eb8fe8ff04f2388c057c22fb8
SRYANG <sarang628@naver.com>
2023-05-12 14:33:09 +0800

    update

--- app/src/main/java/com/sarang/alarm_test_app/MainActivity.kt
+++ app/src/main/java/com/sarang/alarm_test_app/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.sarang.alarm_test_app
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import com.sarang.alarm.fragment.test
 import com.sarang.alarm.uistate.testAlarmUiState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
