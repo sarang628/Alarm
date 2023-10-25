e3b4b70bf21e91180ae1ec0c5dfca2540ad9d294
SRYANG <sarang628@naver.com>
2023-10-25 15:47:27 +0800

    update

--- library/src/main/java/com/sarang/alarm/uistate/TestAlarmUiState.kt
+++ library/src/main/java/com/sarang/alarm/uistate/TestAlarmUiState.kt
@@ -2,20 +2,11 @@ package com.sarang.alarm.uistate
 
 import android.content.Context
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
-import com.example.library.JsonToObjectGenerator
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.lang.reflect.Type
-import java.util.Objects
 
 
 fun testAlarmUiState(context: Context, lifecycleOwner: LifecycleOwner): StateFlow<AlarmUiState> {
@@ -28,7 +19,7 @@ fun testAlarmUiState(context: Context, lifecycleOwner: LifecycleOwner): StateFlo
             //에러메세지 테스트
 //            data.postValue(testErrorOn()); delay(delay); data.postValue(testErrorOff()); delay(delay);
             //리스트 테스트
-            data.emit(testListOn(context)); delay(delay);  //data.postValue(testListOff()); delay(delay);
+            //data.emit(testListOn(context)); delay(delay);  //data.postValue(testListOff()); delay(delay);
         }
     }
     return data;
@@ -50,12 +41,12 @@ fun testErrorOff(): AlarmUiState {
     return AlarmUiState()
 }
 
-fun testListOn(context: Context): AlarmUiState {
+/*fun testListOn(context: Context): AlarmUiState {
     return AlarmUiState(
         list = JsonToObjectGenerator<AlarmListItem>()
             .getListByFile(context, "alarmList.json", AlarmListItem::class.java)
     )
-}
+}*/
 
 fun testListOff(): AlarmUiState {
     return AlarmUiState()
